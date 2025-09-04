7bb5516e7f0397b31035c0f44a90de5b12971e51
IsraelMends <iomendes505@gmail.com>
2025-09-04 17:47:47 -0300

    Feture: Change color service_card

--- src/components/service/service_card/index.tsx
+++ src/components/service/service_card/index.tsx
@@ -5,7 +5,7 @@ interface ServiceCardProps extends Service {}
 const ServiceCard: React.FC<ServiceCardProps> = ({ id, title, description }) => {
   return (
     <div className="bg-gray-50 rounded-lg p-6 hover:shadow-sm shadow-secondary transition-shadow">
-      <div className="w-12 h-12 bg-primary rounded-lg mb-4 flex items-center justify-center text-white text-2xl">
+      <div className="w-12 h-12 bg-secondary rounded-lg mb-4 flex items-center justify-center text-white text-2xl">
         {id}
       </div>
       <h3 className="text-xl text-black font-semibold mb-3">{title}</h3>

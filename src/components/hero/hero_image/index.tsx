f1f115533207233231a95fa95e8aae70f84a881e
IsraelMends <iomendes505@gmail.com>
2025-09-09 12:03:39 -0300

    style(hero): remove bg

--- src/components/hero/hero_image/index.tsx
+++ src/components/hero/hero_image/index.tsx
@@ -1,9 +1,9 @@
 const HeroImage = () => {
   return (
     <div className="md:w-1/2">
-      <div className="bg-gradient-to-r from-blue-500 to-purple-500 rounded-lg p-1">
-        <div className="bg-gray-800 rounded-lg p-8">
-          <div className="aspect-w-16 aspect-h-9 bg-gray-700 rounded-lg ">
+      <div className=" rounded-lg p-1">
+        <div className=" rounded-lg p-8">
+          <div className="aspect-w-16 aspect-h-9 rounded-lg ">
             {/* Placeholder for image or content */}
             <img src="./Global-Full.png" alt="Global logo for hero_content" />
           </div>

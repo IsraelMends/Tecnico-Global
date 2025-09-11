9f1188424c7a82283ca1246044a5bc45f8ef560c
IsraelMends <iomendes505@gmail.com>
2025-09-11 11:41:57 -0300

    refactor(whatsapp): remover importação não utilizada do componente Button

--- src/components/whatsapp_float_button/index.tsx
+++ src/components/whatsapp_float_button/index.tsx
@@ -3,7 +3,6 @@
 import * as React from "react"
 import Image from "next/image"
 import  AppModal  from "@/components/app-modal"
-import { Button } from "@/components/ui/button"
 
 type WhatsAppIconWithModalProps = {
   src?: string

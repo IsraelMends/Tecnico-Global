de93bb48dd62c4916ddc65346138c29fa65e1acf
IsraelMends <iomendes505@gmail.com>
2025-09-12 14:38:30 -0300

    feat(footer-links): atualizar links para navegação correta na seção de rodapé

--- src/components/footer/footer_links/index.tsx
+++ src/components/footer/footer_links/index.tsx
@@ -3,9 +3,9 @@ const FooterLinks = () => {
     <div className="space-y-4">
       <h3 className="text-lg font-semibold">Links</h3>
       <ul className="space-y-2">
-        <li><a href="#About" className="text-gray-400 hover:text-white">Sobre Nós</a></li>
-        <li><a href="#Services" className="text-gray-400 hover:text-white">Serviços</a></li>
-        <li><a href="#Contact" className="text-gray-400 hover:text-white">Contato</a></li>
+        <li><a href="/about" className="text-gray-400 hover:text-white">Sobre Nós</a></li>
+        <li><a href="/#Parceiros" className="text-gray-400 hover:text-white">Parceiros</a></li>
+        <li><a href="/#Contact" className="text-gray-400 hover:text-white">Contato</a></li>
       </ul>
     </div>
   );

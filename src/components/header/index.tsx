677ae40481f7cf16a73177c5b7fe437da16bddba
IsraelMends <iomendes505@gmail.com>
2025-09-12 08:28:16 -0300

    feat(header): remover importação do componente Header_Mob

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,13 +1,11 @@
 "use client";
 
 import Header_Nav from "./header_nav";
-import Header_Mob from "./header_mob";
 
 const Header = () => {
   return (
     <header className="bg-[#241645] shadow-lg">
       <nav className="container mx-auto px-4 py-4">
-
         <Header_Nav />
       </nav>
     </header>

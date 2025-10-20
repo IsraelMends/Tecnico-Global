9de21050818a21b6f92dc29b0983cd771a13767e
IsraelMends <iomendes505@gmail.com>
2025-10-20 17:42:43 -0300

    fix(header-mob): atualizar número do WhatsApp no componente Header_Mob

--- src/components/header/header_mob/index.tsx
+++ src/components/header/header_mob/index.tsx
@@ -93,7 +93,7 @@ const Header_Mob = () => {
       <AppModal
         isOpen={open}
         onClose={() => setOpen(false)}
-        whatsappNumber="5531984770007" // DDI + DDD + número, apenas dígitos
+        whatsappNumber="5594992294042" // DDI + DDD + número, apenas dígitos
         policyUrl="/politica-de-privacidade"
         originLabel="Botão Hero"
       />

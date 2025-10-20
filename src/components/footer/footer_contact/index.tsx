137e2eea36229e8b46e4baf9f37b434a4ef46709
IsraelMends <iomendes505@gmail.com>
2025-10-20 17:42:32 -0300

    fix(footer-contact): atualizar informações de contato no componente FooterContact

--- src/components/footer/footer_contact/index.tsx
+++ src/components/footer/footer_contact/index.tsx
@@ -3,8 +3,7 @@ const FooterContact = () => {
     <div className="space-y-4">
       <h3 className="text-lg font-semibold">Contatos</h3>
       <ul className="space-y-2 text-gray-400">
-        <li>tecnico@faculdade.com</li>
-        <li>+55 (99) 99999-9999</li>
+        <li>+55 (94) 99229-4042</li>
         <li>R. Luiz Rodrigues dos Santos, 44 - Todos Os Santos, Cel. Fabriciano </li>
       </ul>
     </div>
